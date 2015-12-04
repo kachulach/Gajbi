b6119571a389025071f794d99b95b9dbaf95e7b3
Boris Kachulachki <kachulach>
2015-12-04 21:09:21 +0100

    inital commit

---
+++ Gajbi/Gajbi/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Gajbi
+//
+//  Created by Boris Kachulachki on 12/4/15.
+//  Copyright © 2015 Kachulach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Gajbi/Gajbi/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Gajbi
+//
+//  Created by Boris Kachulachki on 12/4/15.
+//  Copyright © 2015 Kachulach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
